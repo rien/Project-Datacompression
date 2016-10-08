513c5cf927f7e858afd6139ec7cbed7657407621
Rien Maertens <maertensrien@gmail.com>
2016-10-08 12:20:50 +0200

    Tweak testfile generator

--- test/test_files/testfile_generator.py
+++ test/test_files/testfile_generator.py
@@ -1,11 +1,30 @@
+#!/bin/python3
+import random
 
-sizes = {'1kb'   : 1000,
-         '1mb'   : 1000000,
-         '10mb'  : 10000000,
-         '100mb' : 100000000,
-         '1gb'   : 1000000000}
+sizes = {'1K'   : 1000,
+         '1M'   : 1000000,
+         '10M'  : 10000000,
+         '100M' : 100000000,
+         '1G'   : 1000000000}
 
+for k, v in sizes.items():
+    file = open(k + '.json', 'w')
 
-for k, v in sizes:
-    file = open(k + '.json')
+    print("Generating " + k + " random numbers")
 
+    rand = random.sample(range(2**63-1), v)
+
+    print("Sorting generated numbers")
+
+    rand_sorted = sorted(rand)
+
+    print("Writing to file")
+
+    file.write("[")
+    for r in rand_sorted[:-1]:
+        file.write(str(r))
+        file.write(", ")
+    file.write(str(rand_sorted[-1]))
+    file.write("]")
+
+    print("Done")
