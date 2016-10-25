e4547c5b603ffb1ec53492fe375fd82b62fd035b
Rien Maertens <maertensrien@gmail.com>
2016-10-25 16:18:10 +0200

    Use huffman in encoder

--- src/standaard/encoder.c
+++ src/standaard/encoder.c
@@ -7,17 +7,21 @@
 #include "../common/constanst.h"
 #include "circular_string.h"
 #include "burrows_wheeler.h"
+#include "move_to_front.h"
+#include "huffman.h"
 
 
 int encode() {
 
-    uchar buffer[BLOCK_SIZE];
-    uchar matrix[BLOCK_SIZE];
+    uchar buffer1[BLOCK_SIZE];
+    uchar buffer2[BLOCK_SIZE];
     size_t a_read;  // amount of bytes read
+    size_t a_encoded;
     size_t t_start; // starting position of the transformed string
-    while((a_read = fread(buffer, BLOCK_SIZE, sizeof(uchar), args.source)) > 0){
-        burrows_wheeler_encode(buffer, a_read, matrix, &t_start);
-        //move_to_front_encode();
+    while((a_read = fread(buffer1, BLOCK_SIZE, sizeof(uchar), args.source)) > 0){
+        burrows_wheeler_encode(buffer1, a_read, buffer2, &t_start);
+        move_to_front_encode(buffer2, buffer1, a_read);
+        uchar* output = huffman_encode(buffer1, a_read, &a_encoded);
 
     }
     if(ferror(args.source)){
