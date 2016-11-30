a43d8cb390c334a04e1b2d41ea11066d8e1dbc60
Rien Maertens <maertensrien@gmail.com>
2016-11-30 20:07:45 +0100

    Other files

--- test/benchmark.sh
+++ test/benchmark.sh
@@ -24,11 +24,11 @@ do
     export orig="files/${file}_randsum.json"
     export comp="compressed/${file}_randsum.json"
     export rest="decompressed/${file}_randsum.json"
-    BENCHMARKFILE=extra/testresults/standaard_compression_randsum.csv ./out/standaard -c $orig $comp
-    BENCHMARKFILE=extra/testresults/standaard_decompression_randsum.csv ./out/standaard -d $comp $rest
+    BENCHMARKFILE=extra/testresults/randsum_standaard_compression.csv ./out/standaard -c $orig $comp
+    BENCHMARKFILE=extra/testresults/randsum_standaard_decompression.csv ./out/standaard -d $comp $rest
     cmp --silent $orig $rest
-    BENCHMARKFILE=extra/testresults/specifiek_compression_randsum.csv ./out/specifiek -c $orig $comp
-    BENCHMARKFILE=extra/testresults/specifiek_decompression_randsum.csv ./out/specifiek -d $comp $rest
+    BENCHMARKFILE=extra/testresults/randsum_specifiek_compression.csv ./out/specifiek -c $orig $comp
+    BENCHMARKFILE=extra/testresults/randsum_specifiek_decompression.csv ./out/specifiek -d $comp $rest
     cmp --silent $orig $rest
     )
 done
@@ -40,11 +40,11 @@ do
     export orig="files/${file}_uniform.json"
     export comp="compressed/${file}_uniform.json"
     export rest="decompressed/${file}_uniform.json"
-    BENCHMARKFILE=extra/testresults/standaard_compression_uniform.csv ./out/standaard -c $orig $comp
-    BENCHMARKFILE=extra/testresults/standaard_decompression_uniform.csv ./out/standaard -d $comp $rest
+    BENCHMARKFILE=extra/testresults/uniform_standaard_compression.csv ./out/standaard -c $orig $comp
+    BENCHMARKFILE=extra/testresults/uniform_standaard_decompression.csv ./out/standaard -d $comp $rest
     cmp --silent $orig $rest
-    BENCHMARKFILE=extra/testresults/specifiek_compression_uniform.csv ./out/specifiek -c $orig $comp
-    BENCHMARKFILE=extra/testresults/specifiek_decompression_uniform.csv ./out/specifiek -d $comp $rest
+    BENCHMARKFILE=extra/testresults/uniform_specifiek_compression.csv ./out/specifiek -c $orig $comp
+    BENCHMARKFILE=extra/testresults/uniform_specifiek_decompression.csv ./out/specifiek -d $comp $rest
     cmp --silent $orig $rest
     )
 done
@@ -58,11 +58,11 @@ do
     export orig="files/${file}_uniform.json"
     export comp="compressed/${file}_uniform.json"
     export rest="decompressed/${file}_uniform.json"
-    BENCHMARKFILE=extra/testresults/standaard_compression_blocksize.csv ./out/standaard -c $orig $comp
-    BENCHMARKFILE=extra/testresults/standaard_decompression_blocksize.csv ./out/standaard -d $comp $rest
+    BENCHMARKFILE=extra/testresults/blocksize_standaard_compression.csv ./out/standaard -c $orig $comp
+    BENCHMARKFILE=extra/testresults/blocksize_standaard_decompression.csv ./out/standaard -d $comp $rest
     cmp --silent $orig $rest
-    BENCHMARKFILE=extra/testresults/specifiek_compression_blocksize.csv ./out/specifiek -c $orig $comp
-    BENCHMARKFILE=extra/testresults/specifiek_decompression_blocksize.csv ./out/specifiek -d $comp $rest
+    BENCHMARKFILE=extra/testresults/blocksize_specifiek_compression.csv ./out/specifiek -c $orig $comp
+    BENCHMARKFILE=extra/testresults/blocksize_specifiek_decompression.csv ./out/specifiek -d $comp $rest
     cmp --silent $orig $rest
     )
 done
